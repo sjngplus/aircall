479a954d0c03b783a806467a67536ff76b736075
SJ <sj.jega2@gmail.com>
2021-11-18 22:40:32 -0500

    fetch data from api

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Activity = () => {
+
+  useEffect(() => {
+    const cancelTokenSource = axios.CancelToken.source();
+    const url = `https://aircall-job.herokuapp.com/activities`;
+    console.log("##Fetching data from API##");
+    axios.get(url, {cancelToken: cancelTokenSource.token})      
+    .then(res => console.log(res.data))
+    .catch(err =>console.log(err));
+
+    return () => cancelTokenSource.cancel(); 
+  }, [])
+
   return (
     <div>
       Activity body
---
+++ src/components/CallItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CallItem = () => {
+  return (
+    <div>
+      A call
+    </div>
+  )
+}
+
+export default CallItem
