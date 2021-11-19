410e0136f6deca912b4bb8a81bb3a06e5b8be2fb
SJ <sj.jega2@gmail.com>
2021-11-19 14:01:59 -0500

    stylize and show time and date of the call

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -2,19 +2,26 @@ import React from 'react'
 import Accordion from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button';
 import { VscCallIncoming, VscCallOutgoing } from 'react-icons/vsc';
+import { MdAccessTime } from 'react-icons/md';
 
-const Activity = ({call}) => {  
+const Activity = ({call}) => {
+
+  const parsedTime = new Date(call.created_at).toLocaleTimeString("en-US");
+  
+  const DateOptions = { year: 'numeric', month: 'short', day: 'numeric'};
+  const parsedDate = new Date(call.created_at).toLocaleDateString("en-US", DateOptions);
 
   return (    
     <Accordion.Item eventKey={call.id}>
       <Accordion.Header>              
         {call.direction === "outbound" ? 
-          <VscCallOutgoing style={{maxHeight: '20px', maxWidth: '20px', marginRight: '10px'}}/> : <VscCallIncoming style={{maxHeight: '20px', maxWidth: '20px', marginRight: '10px'}}/>} 
-        {call.direction === "outbound" ? call.to : call.from}
+          <VscCallOutgoing style={{maxHeight: '15px', maxWidth: '15px', marginRight: '5px'}}/> : <VscCallIncoming style={{maxHeight: '15px', maxWidth: '15px', marginRight: '10px'}}/>} 
+        {call.direction === "outbound" ? call.to : call.from} 
+        <MdAccessTime style={{maxHeight: '15px', maxWidth: '15px', marginLeft: '20px', marginRight: '5px'}}/> {parsedTime}
       </Accordion.Header>
-      <Accordion.Body>
+      <Accordion.Body className="px-1">
         <div className="call-body">
-          <p>Call status: {call.call_type} call | Call Via: {call.via}</p>
+          <p>Call status: {call.call_type} call | Call Date: {parsedDate}</p>
           <p>Duration: {call.duration} seconds | From: {call.from ? call.from : "N/A"}</p>
           <p style={{marginBottom: '0px'}}><Button variant="outline-primary" size="sm">Archive</Button> </p>
         </div>
